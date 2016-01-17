28cee1aea0958526838fcd51927316cdddd2abbf
kapilmohan <kapilonweb@gmail.com>
2016-01-18 01:52:07 +0530

    using omniauth and koala for FB login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,2 +1,12 @@
 class LoginController < ApplicationController
+
+    def index
+
+    end
+
+    def login
+        @user = User.koala(request.env['omniauth.auth']['credentials'])
+
+        #redirect to survey#index
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get 'survey' => 'survey#index'
   get 'survey/captureresponse' => 'survey#captureresponse'
 
+  get 'auth/facebook', as: "auth_provider"
+  get 'auth/facebook/callback', to: 'login#login'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
